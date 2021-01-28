e3f1aa5289c27c54e4606a35be32f2bef15ac9ae
Obierefu Finbarr Chukwuka <57522378+finbarr91@users.noreply.github.com>
2021-01-27 21:00:11 -0800

    initial commit

--- investment.py
+++ investment.py
@@ -1,1 +1,23 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Let's look at the dataset
+companies = pd.read_csv('companies.txt', sep='\t', encoding= 'ISO-8859-1')
+rounds = pd.read_csv('rounds2.csv', encoding = 'ISO-8859-1')
+
+# Looking at the dataset head
+print(rounds.head())
+print(rounds.info(), '\n')
+print(rounds.shape)
+
+# Lets look at the companies head
+print(companies.head())
+print(companies.info())
+print(companies.shape)
+
+# identify the unique number of permalinks in companies
+print(len(companies.permalink.unique()))
+
 
